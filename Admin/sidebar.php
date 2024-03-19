5895a3526b50e9e0bc1cb9380499117920835107
thanhphat113 <thanhphat9523@gmail.com>
2024-03-19 15:46:30 +0700

    sidebar

---
+++ Admin/sidebar.php
@@ -0,0 +1,15 @@
+<div class="sidebar">
+	<div class="top">
+		<p align="center">Admin</p><hr>
+		<ul>
+			<li><a href="#">Quản lý sản phẩm</a></li>
+			<li><a href="#">Quản lý nhân viên</a></li>
+			<li><a href="#">Quản lý doanh thu</a></li>
+			<li><a href="#">Quản lý tài khoản</a></li>
+			<li><a href="#">Thống kê</a></li>
+		</ul>
+	</div>
+	<div class="bottom">
+		<p>Xin chào,</p>
+	</div>
+</div>
