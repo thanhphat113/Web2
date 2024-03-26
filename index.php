eab90d0bb3a26a4da6e3ec1fc6fcff494262fe26
thanhphat113 <thanhphat9523@gmail.com>
2024-03-26 14:26:10 +0700

    ...

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-LYrvSR95rNk45vtQodYJ1uBUy4Phl3EamWc8o39z+PgaV3BFlaTf8bEjkI1nLMnxlN+5+G6o70UvEMegiN9zVw==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 	<link rel="stylesheet" href="styles.css">
 	<title>Infinity Store</title>
 </head>
