21b240541e60afef28438978b95df650ed5399a0
thanhphat113 <thanhphat9523@gmail.com>
2024-04-10 00:13:09 +0700

    ...

--- Admin/thongke.php
+++ Admin/thongke.php
@@ -1,26 +1,56 @@
+<?php
+	require("./database/connection.php");
+?>
 
 <div class="top">
 	<a href="index.php?id=sanpham" class="box box1">
 			<span class="title-box">Sản Phẩm</span>
-			<p class="box-content">100</p>
+			<p class="box-content"><?php echo countProducts();?></p>
 	</a>
 	<a href="index.php?id=khachhang" class="box box2">
 			<span class="title-box">Khách Hàng</span>
-			<p class="box-content">100</p>
+			<p class="box-content"><?php echo countCus(); ?></p>
 	</a>
 	<a href="index.php?id=nhanvien" class="box box3">
 			<span class="title-box">Nhân viên</span>
-			<p class="box-content">100</p>
+			<p class="box-content"><?php echo countEmployee(); ?></p>
+	</a>
 	<a href="#bottom" class="box box4">
 			<span class="title-box">Doanh Thu</span>
 			<p class="box-content">100</p>
 	</a>
 </div>
 <div class="center">
-		<div class="boxex">	
-			<div class="title-box">
-				<h3><i class="far fa-money-bill-alt"></i> Danh sách hoá đơn</h3>
+		<div class="boxex">
+			<div class="box-title">
+				<h1><i class="far fa-money-bill-alt"></i> Danh sách hoá đơn</h1>
 			</div>
+			<?php $hoadon_list = HoaDon_list(); ?>
+				<table  class = "table_view">
+					<thead>
+						<tr>
+						<th>Mã hoá đơn</th>
+						<th>Nhân viên </th>
+						<th>Khách hàng</th>
+						<th>Mã KM</th>
+						<th>Ngày tạo</th>
+						<th>Tổng tiền</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php foreach ($hoadon_list as $hoadon) {
+									echo'<tr> 
+											<td>'.$hoadon["MaHD"].'</td> 
+											<td type="left">'.$hoadon["TenNV"] .'</td>
+											<td type="left">'.$hoadon["TenKH"] .'</td>
+											<td>'.$hoadon["MaKM"] .'</td>
+											<td>'.$hoadon["NgayTao"] .'</td>
+											<td>'.addFormat($hoadon["TongTien"]).'</td>
+											</tr>';
+									}
+						?>
+					</tbody>
+				</table>
 		</div>
 		<div class="boxex">
 			<h1><i class="fas fa-star" style="color: #FFD43B;"></i> Top sản phẩm bán chạy</h1><hr>
@@ -40,7 +70,3 @@
 		<h3><i class="fas fa-chart-pie"></i> Thị phần các loại sản phẩm bán được</h3><hr>
 	</div>
 </div>
-
-
-
-
--- database/connection.php
+++ database/connection.php
@@ -1,31 +1,88 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "quanlycuahang";
+function connectToDatabase() {
+    // Thông tin kết nối đến cơ sở dữ liệu
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "cuahangdienthoai";
 
-// Tạo kết nối
-$conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $database);
 
-// Kiểm tra kết nối
-if ($conn->connect_error) {
-    die("Kết nối không thành công: " . $conn->connect_error);
+    if ($conn->connect_error) {
+        die("Kết nối thất bại: " . $conn->connect_error);
+    }
+    return $conn;
 }
 
-function findAllProducts($conn) {
-    // Truy vấn để lấy tất cả các sản phẩm từ bảng products
-    $sql = "SELECT * FROM products";
-    $result = $conn->query($sql);
+function addFormat($number){
+    $number = number_format($number, 1, '.', ','). ' <sup>đ</sup>';
+    return $number;
+}
 
-    // Kiểm tra và trả về kết quả nếu truy vấn thành công
-    if ($result->num_rows > 0) {
-        $products = array();
-        while($row = $result->fetch_assoc()) {
-            $products[] = $row;
-        }
-        return $products;
+
+function countEmployee(){
+    $conn = connectToDatabase();
+	$query = "SELECT COUNT(*) as total FROM nhanvien";
+    $result = $conn->query($query);
+    $conn->close();
+
+	if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        return $row['total'];
     } else {
-        return []; // Trả về một mảng rỗng nếu không có sản phẩm nào được tìm thấy
+        return 0;
     }
+
+}
+
+function countCus(){
+    $conn = connectToDatabase();
+	$query = "SELECT COUNT(*) as total_customers FROM khachhang";
+    $result = $conn->query($query);
+    $conn->close();
+
+	if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        return $row['total_customers'];
+    } else {
+        return 0;
+    }
+}
+
+function countProducts(){
+    $conn = connectToDatabase();
+	$query = "SELECT COUNT(*) as total FROM sanpham";
+    $result = $conn->query($query);
+
+	if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        return $row['total'];
+    } else {
+        return 0;
+    }
+    $conn->close();
+}
+
+function HoaDon_list(){
+    $list = array();
+    $conn = connectToDatabase();
+    $query = "SELECT hd.MaHD,nv.TenNV,kh.TenKH,hd.MaKM,hd.NgayTao,hd.TongTien FROM hoadon as hd join nhanvien as nv on nv.MaNV=hd.MaNV
+    join khachhang as kh on kh.MaKH=hd.MaKH";
+    $result = $conn->query($query);
+    $conn->close();
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $hoadon = array(
+                "MaHD" => $row["MaHD"],
+                "TenNV" => $row["TenNV"],
+                "TenKH" => $row["TenKH"],
+                "MaKM" => $row["MaKM"],
+                "NgayTao" => $row["NgayTao"],
+                "TongTien" => $row["TongTien"]
+            );
+            array_push($list, $hoadon);
+        }
+    }
+    return $list;
 }
 ?>
